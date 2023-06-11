20908cdef34018f4fdf1cddb065ac59e4d5756d0
Ashutosh JHa <ashutoshj.cs.21@nitj.ac.in>
2023-06-11 19:23:33 +0545

    added few stuffs

--- job.py
+++ job.py
@@ -1,12 +1,13 @@
 from bs4 import BeautifulSoup
 # scrapes latest published job advertisement from a specific website and returns the job title and the link to the job
 import requests
-import time
-import datetime
-import smtplib
-from email.message import EmailMessage
-import os
+
 
 # the url of the website to be scraped
-html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=usa').text
+html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from'
+                         '=submit&txtKeywords=python&txtLocation=usa').text
 print(html_text)
+soup = BeautifulSoup(html_text, 'lxml')
+# print(soup.prettify())
+jobs = soup.find_all('li', class_="clearfix job-bx wht-shd-bx")
+print(jobs)
